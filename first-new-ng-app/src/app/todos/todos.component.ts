ec871608f83ad2e6b45ac486e89810fea3c332ce
Rohit Kandpal <iamrohitkandpal@gmail.com>
2025-09-01 02:54:25 +0530

    Just completed with the structure of the framework and it's working interesting. Will make a project now

--- first-new-ng-app/src/app/todos/todos.component.ts
+++ first-new-ng-app/src/app/todos/todos.component.ts
@@ -2,20 +2,47 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { TodosService } from '../services/todos.service';
 import { Todo } from '../model/todo.type';
 import { TodoItemComponent } from '../components/todo-item/todo-item.component';
+import { catchError } from 'rxjs';
+import { FormsModule } from '@angular/forms';
+import { FilterTodosPipe } from '../pipes/filter-todos.pipe';
 
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [TodoItemComponent],
-  templateUrl: './todos.component.html',
+  imports: [TodoItemComponent, FormsModule, FilterTodosPipe],
+templateUrl: './todos.component.html',
   styleUrl: './todos.component.scss'
 })
 export class TodosComponent implements OnInit {
   todosService = inject(TodosService);
   todoItems = signal<Array<Todo>>([]);
+  searchTerm = signal('');
 
   ngOnInit(): void {
-    console.log(this.todosService.todoItems);
-    this.todoItems.set(this.todosService.todoItems);
+    this.todosService
+      .getTodosFromApi()
+      .pipe(
+        catchError((err) => {
+          console.log(err);
+          throw err;
+        })
+      )
+      .subscribe((todos) => {
+        this.todoItems.set(todos);
+      })
   }
+
+  todoUpdate(todoItem: Todo) {
+    this.todoItems.update((todos) => {
+      return todos.map(todo => {
+        if(todo.id === todoItem.id) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          }
+        } 
+        return todo;
+      })
+    })
+  } 
 }
